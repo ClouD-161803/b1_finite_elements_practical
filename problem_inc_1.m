5674d50835cc8c070bde675acf2beb2970a2cdc0
claudio vestini <cvestini@gmail.com>
2024-12-05 13:28:05 +0000

    Add report

--- problem_inc_1.m
+++ problem_inc_1.m
@@ -3,14 +3,17 @@
 clear; close all;
 %% Mesh generation
 elementType='2dQ1'; %define element type
-% * MODIFICATION: Define boundary element type
+
+% * ADD: Define boundary element type
 elementType1d='1dQ1'; % consistent with elementType
 % *
+
 domain=[0 0 4 1]; %[x0 y0 x1 y1]
 numElements=[15 10]; %[numElX numElY]
 [nodeCoords, IEN, boundaryElementIDs, boundaryNodeLocalID]=...
     meshRect2d(domain,elementType,numElements); %generate the mesh
-% * MODIFICATION: Create boundary incidence arrays
+
+% * ADD: Create boundary incidence arrays
 BIEN=IENtoBIEN(IEN, boundaryElementIDs, boundaryNodeLocalID);
 % *
 %% Parameters
@@ -26,20 +29,41 @@ K = formStiffnessMatrixEng(nodeCoords, IEN, elementType, numGP, CMatrix);
 % Body force
 Fb = zeros(numDoFs,1);
 
-% Boundary conditions
-leftNodes=find(nodeCoords(:,1)==domain(1)); 
-rightNodes=find(nodeCoords(:,1)==domain(3));
-leftXDoF=(leftNodes-1)*2+1;
-leftYDoF=(leftNodes-1)*2+2;
-rightXDoF=(rightNodes-1)*2+1;
-rightYDoF=(rightNodes-1)*2+2;
+% * ADD: Boundary conditions
+%define function handles for BC
+bndDisplacement=@(x)(zeros(size(x))'); % zero displacement
+bndTraction0=@(x)(zeros(size(x))'); % zero traction
+% Traction vector [1; 0] for the right boundary
+bndTraction1=@(x)(repmat([1; 0],size(x,1)));  % non-zero traction
+%make cell arrays of function handles - boundary condition cell arrays must
+%match the structure of BIEN 
+bndTractions={bndTraction0,bndTraction1,bndTraction0,bndTraction0};
+bndDisplacements=repmat({bndDisplacement},4,1); 
+isDirichlet=[0; 1; 0; 1]; %define which boundary regions have Dirichlet BC
 
-prescribedDoF=[leftXDoF; leftYDoF]; %list of prescribed global DoFs
-freeDoF=setdiff(1:numDoFs,prescribedDoF); %list of free DoFs
-u_prescribed=zeros(numDoFs,1); 
-% Surface traction
-Fs = zeros(numDoFs,1); %define global force vector for surface traction
+numGP1d=1; %number of Gauss Points for 1d boundary elements
+% assemble boundary load vector for Neumann BC
+% and evaluate displacements for Dirichlet BC
+[u_prescribed, Fs, prescribedDoF, freeDoF]=...
+    formBC(nodeCoords,BIEN,elementType1d,numGP1d,...
+    bndTractions,bndDisplacements,isDirichlet);
+% *
 
+% ! REMOVE: Remove old boundary conditions
+% % Boundary conditions
+% leftNodes=find(nodeCoords(:,1)==domain(1)); 
+% rightNodes=find(nodeCoords(:,1)==domain(3));
+% leftXDoF=(leftNodes-1)*2+1;
+% leftYDoF=(leftNodes-1)*2+2;
+% rightXDoF=(rightNodes-1)*2+1;
+% rightYDoF=(rightNodes-1)*2+2;
+
+% prescribedDoF=[leftXDoF; leftYDoF]; %list of prescribed global DoFs
+% freeDoF=setdiff(1:numDoFs,prescribedDoF); %list of free DoFs
+% u_prescribed=zeros(numDoFs,1); 
+% % Surface traction
+% Fs = zeros(numDoFs,1); %define global force vector for surface traction
+% !
 %% Solution 
 F=Fb+Fs; %total load vector
 %define the free part of load vector
@@ -74,4 +98,5 @@ hold on;
 factor=1e7; %scaling factor to amplify small deformations
 %draw deformed mesh
 drawElements(nodeCoords+u2*factor,IEN,elementType,s2(1,:)',.7);
+drawNodes(nodeCoords,BIEN{4},{'ks','filled'}); %draw pinned nodes
 title('\sigma_x')
